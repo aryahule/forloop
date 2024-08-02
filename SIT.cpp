6338ad434287214e37101015f031cbb8f19c8662
aryahule <aryahule27@gmail.com>
2024-08-02 11:19:35 +0530

    Add files via upload

---
+++ SIT.cpp
@@ -0,0 +1,11 @@
+//ARYA HULE 
+//23070123029
+using namespace std;
+
+int main() {
+    for(int i = 0 ; i < 5 ; i++){
+        cout << "SIT" <<endl;
+    }
+
+    return 0;
+}
---
+++ even.cpp
@@ -0,0 +1,14 @@
+//ARYA HULE 
+//23070123029
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i =0 ; i <= 10 ; i++ ){
+        if(i%2==0){
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
